ea4b2bdb4dcddc4704c49b42347bc6a436de8d01
xijie <xijie@sit.com.cn>
2021-01-21 01:14:37 +0800

    提交人：大圣 修改描述：springcloud父工程及8001支付模块搭建

---
+++ cloud-provider-payment8001/src/main/java/com/xj/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.xj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xijie
+ * @version V1.0
+ * Date 2021/1/21 0:44
+ * @Description:
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
